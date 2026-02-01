48003789a16633400bf406d57afe01e120f383fe
user <akparaliyevikromjon7@gmail.com>
2026-02-01 17:13:42 +0500

    added

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -6,9 +6,16 @@ const Sidebar = ({data}) => {
     <div>
       {data.map((item ,index)=>
       <div key={index} className="sidebar">
+        <div className="sidebar_header">
         <img src={item.images} alt="img" />
         <h1>{item.headtitle}</h1>
+        </div>
+        <div style={{display: 'flex', alignItems: 'center', justifyContent:'flex-start'}} className="menu">
+          <img src={item.img} alt="images" />
+          <p>{item.title}</p>
+        </div>
       </div>
+      
       )}
     </div>
   );
